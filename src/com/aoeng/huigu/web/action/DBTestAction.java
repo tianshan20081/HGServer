34f68effa7ba4a1b6f07891ee2a72435aa33570f
tianshan20081 <zhangshch2008@gmail.com>
2014-10-16 07:37:20 +0800

    update

---
+++ src/com/aoeng/huigu/web/action/DBTestAction.java
@@ -0,0 +1,18 @@
+package com.aoeng.huigu.web.action;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("dbtest")
+public class DBTestAction extends BaseAction<Object> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4835187334378129095L;
+
+	
+	public void test(){
+		
+		
+	}
+}
